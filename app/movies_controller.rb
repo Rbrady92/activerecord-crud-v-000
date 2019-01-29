94bc31e961355af2981815b5180633928e5accfd
Ryan Brady <ryanbrady369@gmail.com>
2019-01-29 22:37:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,12 +34,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  movie = Movie.first.title
-  movie
+  Movie.find_by(id: 1)
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  movie = Movie.last.title
 end
 
 def can_get_size_of_the_database
